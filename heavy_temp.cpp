b4b3ba16fdc789be0f48822a1d863edcaa345f3a
Navoditn <39704421+navodit15@users.noreply.github.com>
2019-11-07 17:16:09 +0530

    Update heavy_temp.cpp

--- heavy_temp.cpp
+++ heavy_temp.cpp
@@ -14,17 +14,13 @@ const int weirdmod=998244353;
 #define MOD 1000000007
 #define sd(x) scanf("%d", &(x))
 #define mp make_pair
-#define mt make_tuple
 #define fi first
 #define se second
 #define pb push_back
-#define eb emplace_back
-#define ones(x) __builtin_popcount(x)
 #define sz(x) int (x.size())
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
-#define REP(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
 #define f1(i,a,b) for(int i=a; i<=b; i++)
 #define rep(i,a,b) for(int i=a; i<b; i++)
 #define NFOR(i,a,b) for(int i=(a);i>=(b);--i)
@@ -44,42 +40,39 @@ typedef vector<ll> vll;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 // order_of_key (val): returns the no. of values less than val
 // find_by_order (k): returns the iterator to kth largest element.(0-based)
- 
- 
-template<class T> bool uin(T &a, T b) { return a > b ? (a = b, true) : false; }
-template<class T> bool uax(T &a, T b) { return a < b ? (a = b, true) : false; }
+
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
  
+
+auto clk=clock();
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+} 
 
 
 template<typename T>
     inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v){
         bool first = true;
         os << "[";
-        for(unsigned int i = 0; i < v.size(); i++)
-        {
-            if(!first)
-                os << ", ";
+        for(unsigned int i = 0; i < v.size(); i++){
+            if(!first) os << ", ";
             os << v[i];
             first = false;
         }
         return os << "]";
     }
+    
 template<class T,class U>
-ostream& operator<<(ostream& os,const pair<T,U>& p){
-	os<<"("<<p.first<<", "<<p.second<<")";
-	return os;
-}
+ostream& operator<<(ostream& os,const pair<T,U>& p){ os<<"("<<p.first<<", "<<p.second<<")"; return os;}
 
 template<typename T>
     inline std::ostream &operator << (std::ostream & os,const std::set<T>& v){
-        bool first = true;
-        os << "[";
-        for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
-        {
-            if(!first)
-                os << ", ";
+        bool first = true;os << "[";
+        for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii){
+            if(!first)os << ", ";
             os << *ii;
             first = false;
         }
@@ -87,16 +80,10 @@ template<typename T>
 }
 
 template<class T,class U>
-ostream& operator<<(ostream& os,const map<T, U> & mp){
-	os << "{";
-	int i = 0;
-	for(auto it : mp){
-		if(i) os<<", ";
-		os << it;
-		i++;
-	}
-	os << "}";	
-	return os;
+ostream& operator<<(ostream& os,const map<T, U> & mp){os << "{";
+    int i = 0;
+    for(auto it : mp){  if(i) os<<", "; os << it; i++;} os << "}";    
+    return os;
 }
 
 
@@ -105,25 +92,20 @@ ostream& operator<<(ostream& os,const map<T, U> & mp){
 #define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
 template<typename Arg1>
 void __f(const char *name, Arg1 &&arg1) {
-	cout<<name<<" : "<<arg1<<endl;
+    cout<<name<<" : "<<arg1<<endl;
 }
 template<typename Arg1, typename ... Args>
 void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-	const char *comma=strchr(names+1,',');
-	cout.write(names,comma-names)<<" : "<<arg1<<" | ";
-	__f(comma+1,args...);
+    const char *comma=strchr(names+1,',');
+    cout.write(names,comma-names)<<" : "<<arg1<<" | ";
+    __f(comma+1,args...);
 }
 #else
 #define trace(...)
 #define endl '\n'
 #endif
  
-auto clk=clock();
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-	uniform_int_distribution<int> uid(0,lim-1);
-	return uid(rang);
-}
+
  
 void solve(){
 
@@ -131,15 +113,15 @@ void solve(){
 }
  
 signed main(){
-	start()
-	int T =1 ; 
-	//cin>>T;
-	while(T--){
-		solve();
-	}
+    start()
+    int T =1 ; 
+    //cin>>T;
+    while(T--){
+        solve();
+    }
  
 #ifdef np
-	cout<<endl<<endl<<endl<<endl<<"Time elapsed: "<<(double)(clock()-clk)/CLOCKS_PER_SEC<<endl;
+    cout<<endl<<endl<<endl<<endl<<"Time elapsed: "<<(double)(clock()-clk)/CLOCKS_PER_SEC<<endl;
 #endif
-	return 0;
+    return 0;
 }
