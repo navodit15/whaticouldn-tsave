7e1e4c5a1f7d5720caa5d0a7a13770e4d7811a11
Navoditn <39704421+navodit15@users.noreply.github.com>
2019-12-14 15:40:59 +0530

    Update lite_temp.cpp

--- lite_temp.cpp
+++ lite_temp.cpp
@@ -1,67 +1,41 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("O3")
-#pragma GCC optimize("Ofast")
- 
-const long long infl=0x3f3f3f3f3f3f3f3fLL;
-const int infi=0x3f3f3f3f;
-const int modo=998244353; 
-#define start() ios::sync_with_stdio(false); cin.tie(nullptr);
-#define MOD 1000000007
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define eb emplace_back
-#define sz(x) int (x.size())
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define rep(i,a,b) for(int i=a; i<b; i++)
-#define NFOR(i,a,b) for(int i=(a);i>=(b);--i)
-  
-using ll =  long long ;
+
 using namespace std;
- 
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector<pii> vpi;
-typedef double ld;
-typedef vector<ll> vll;
- 
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
- 
 
 #ifdef np
 #define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
-template<typename Arg1>
-void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
-template<typename Arg1, typename ... Args>
-void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-    const char *comma=strchr(names+1,',');
-    cout.write(names,comma-names)<<" : "<<arg1<<" | ";
+template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
+template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
     __f(comma+1,args...);
 }
 #else
 #define trace(...)
 #define endl '\n'
 #endif
- 
 
- 
-void solve(){
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
+	
     
-}
- 
-signed main(){
-    start()
-    int T =1 ; 
-    //cin>>T;
-    while(T--){
-        solve();
-    }
- 
+    
+
 #ifdef np
     cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif
-    return 0;
-}
+#endif  
+ return 0; 
+ }
