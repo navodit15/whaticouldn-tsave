68145e9e4d0c3526363dc30965ff8230c45b4711
Navoditn <39704421+navodit15@users.noreply.github.com>
2019-12-18 10:39:49 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -14,13 +14,6 @@ typedef vector<int> vi;
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
 
-#ifdef np
-#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
-template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
-template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
-    __f(comma+1,args...);
-}
 template<class T> ostream& operator<<(ostream &os, vector<T> V) {
     os << "[ "; for(auto v : V) os << v << " "; return os << "]";
 }
@@ -32,6 +25,13 @@ template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
 }
 template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
     os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";
+}
+#ifdef np
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
+template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
+    __f(comma+1,args...);
 }
 #else
 #define trace(...)
