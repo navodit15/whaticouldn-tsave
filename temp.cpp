afdfb55760821c35537a8879fbb667875dacafe2
Navoditn <39704421+navodit15@users.noreply.github.com>
2019-12-18 10:35:24 +0530

    template

---
+++ temp.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
+
+#ifdef np
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
+template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
+    __f(comma+1,args...);
+}
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {
+    os << "[ "; for(auto v : V) os << v << " "; return os << "]";
+}
+template<class T> ostream& operator<<(ostream &os, set<T> S){
+    os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";
+}
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")";
+}
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
+    os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";
+}
+#else
+#define trace(...)
+#define endl '\n'
+#endif
+
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
+	
+
+    
+    
+
+#ifdef np
+    cerr <<endl<<endl<< "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif  
+ return 0; 
+ }
