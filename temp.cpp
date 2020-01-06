7adbedfd730fa2e8fed81c1ec4231e09d406e49e
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-01-06 13:01:45 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -17,7 +17,6 @@ template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for
 template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
 template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
 template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
-template<typename T>ostream &operator << (ostream &os,vector<vector<T>>& A){for (auto &B: A) {os << '\n' << B;} return os; }
 #ifdef np
 #define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
 template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
